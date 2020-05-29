fd0cfabadfe67a2f842898e8f2616056828bc61c
Jellestiesri <you@example.comjellestiesri@gmail.com>
2020-05-29 17:20:29 +0200

    Summatief 3 af

--- Summatief3/src/Main.java
+++ Summatief3/src/Main.java
@@ -55,7 +55,6 @@ public class Main {
             Node einde = nodeE;
 
             graaf =  Dijkstra.calculateShortestPathFromSource(graaf, begin, einde); // Voert Dijkstra functie uit
-            System.out.println("gra"+graaf);
             Reis reis1 = new Reis(nodeE); // Nieuwe reis aanmaken
             reis1.setStapReis(reis1); // Set de reis van de stappen in reis1 naar reis1
 
